a790c62c3d721bce146910601b5bd4aa36570c31
KseniaKrasilnikova <tksenia13@gmail.com>
2021-08-24 18:23:59 +0300

    done accordeon with no-js state

--- source/js/script.js
+++ source/js/script.js
@@ -1,25 +1,29 @@
 'use strict';
 
-var accordeonButtons = document.getElementsByClassName('accordeon__btn');
-for (var i = 0; i < accordeonButtons.length; i++) {
-  accordeonButtons[i].addEventListener('click', function () {
-    for (var j = 0; j < accordeonButtons.length; j++) {
-      accordeonButtons[j].classList.remove('accordeon--active');
-    }
-    this.classList.toggle('accordeon--active');
-  });
-}
-/*
-var accordeonMapBtn = document.querySelector('.accordeon__btn--map');
-var accordeonAddressBtn = document.querySelector('.accordeon__btn--address');
-var accordeonMapContent = document.querySelector('.cite-map__content');
-var accordeonAddressContent = document.querySelector('.address__content');
+// accordeon in footer
+var setupAccordeon = function () {
+  var accordeonButtons = document.getElementsByClassName('accordeon__btn');
+  for (var i = 0; i < accordeonButtons.length; i++) {
+    accordeonButtons[i].addEventListener('click', function () {
 
-accordeonMapBtn.addEventListener('click', function () {
-  if (accordeonMapContent.classList.contains('cite-map--closed')) {
-    accordeonMapContent.classList.remove('cite-map--closed');
-    accordeonMapContent.classList.add('cite-map--opened');
-  } else {
-    accordeonMapContent.classList.remove('cite-map--opened');
+      for (var j = 0; j < accordeonButtons.length; j++) {
+        if (accordeonButtons[j] !== this) {
+          accordeonButtons[j].classList.remove('accordeon--active');
+        }
+      }
+      this.classList.toggle('accordeon--active');
+    });
   }
-});*/
+};
+
+
+// no-JS in accordion
+var jsOn = function () {
+  var elements = document.querySelectorAll('.no-js');
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].classList.remove('no-js');
+  }
+};
+
+jsOn();
+setupAccordeon();
